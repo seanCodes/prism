fff938e8e3466dc24a4813851ef9f898b24f37ef
Sean Juarez <seanjcodes@gmail.com>
2013-06-14 11:14:31 -0600

    add regex for LESS functions

--- components/prism-less.js
+++ components/prism-less.js
@@ -48,6 +48,7 @@ Prism.languages.less = {
 	'string': /("(?:(?!")[^\\]|\\.)*"|'(?:(?!')[^\\]|\\.)*')/g,
 	'keyword': /\b(when)\b/g,
 	'color': /(#[a-fA-F0-9]{8}|#[a-fA-F0-9]{6}|#[a-fA-F0-9]{3})(\b|$)/mg,
+	'function': /\b(escape|e|%|unit|color|data-uri|ceil|floor|percentage|round|sqrt|abs|sin|asin|cos|acos|tan|atan|pi|pow|mod|convert|rgb|rgba|argb|hsl|hsla|hsv|hsva|hue|saturation|lightness|hsvhue|hsvsaturation|hsvvalue|red|green|blue|alpha|luma|saturate|desaturate|lighten|darken|fadein|fadeout|fade|spin|mix|greyscale|contrast|multiply|screen|overlay|softlight|hardlight|difference|exclusion|average|negation|iscolor|isnumber|isstring|iskeyword|isurl|ispixel|ispercentage|isem|isunit)(?=\s*\()/g,
 	'number': {
 		pattern: /(^|[^\w-])-?\d*\.?\d+(px|%|em|rem|pc|pt|ex|in|cm|mm|vw|vh|deg|s|ms)?(?=[^\w-])/g,
 		lookbehind: true
