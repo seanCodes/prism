dc13c8fb721f528f3b6c2321cc3bb061a06e45df
Sean Juarez <seanjcodes@gmail.com>
2013-06-14 11:18:08 -0600

    add at-rule regex

--- components/prism-less.js
+++ components/prism-less.js
@@ -47,6 +47,7 @@ Prism.languages.less = {
 	'comment': /(\/\*(?:[^*]|\*+[^\/*])*\*+\/|\/\/[^\n]*)/mg,
 	'string': /("(?:(?!")[^\\]|\\.)*"|'(?:(?!')[^\\]|\\.)*')/g,
 	'keyword': /\b(when)\b/g,
+	'atrule': /@(charset|font-face|import|keyframes|media|page)[\w\s]*(?=\(|\{)/gi,
 	'function': /\b(escape|e|%|unit|color|data-uri|ceil|floor|percentage|round|sqrt|abs|sin|asin|cos|acos|tan|atan|pi|pow|mod|convert|rgb|rgba|argb|hsl|hsla|hsv|hsva|hue|saturation|lightness|hsvhue|hsvsaturation|hsvvalue|red|green|blue|alpha|luma|saturate|desaturate|lighten|darken|fadein|fadeout|fade|spin|mix|greyscale|contrast|multiply|screen|overlay|softlight|hardlight|difference|exclusion|average|negation|iscolor|isnumber|isstring|iskeyword|isurl|ispixel|ispercentage|isem|isunit)(?=\s*\()/g,
 	'color': /(#[a-fA-F0-9]{8}|#[a-fA-F0-9]{6}|#[a-fA-F0-9]{3})/g, //|[hr][bglsv]{2}a?
 	'number': {
